d13925d3d6f2653f6727b6d5ed782a1f0284b8f6
Guido Rocco <guido.rocco@hotmail.it>
2017-08-13 15:45:06 +0200

    Update diff_words.cs

--- diff_words.cs
+++ diff_words.cs
@@ -1,3 +1,8 @@
+/* 
+Problem: Given two strings, a and b, that may or may not be of the same length, determine the minimum number of character deletions
+required to make a and b anagrams. Any characters can be deleted from either of the strings. 
+We can suppose that a and b have at least one letter in common. 
+*/ 
 using System;
 using System.Collections.Generic;
 using System.IO;
