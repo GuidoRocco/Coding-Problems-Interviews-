7949ccb1de13a51c06d9fd19d649633c18ae3f55
Guido Rocco <guido.rocco@hotmail.it>
2017-08-24 16:41:43 +0200

    Create substitute_string.cpp

---
+++ substitute_string.cpp
@@ -0,0 +1,18 @@
+* 
+Write a function in C++ that substitute all the occurrences of a given string with another given string. 
+*/ 
+
+#include <iostream> 
+#include <algorithm> 
+using namespace std; 
+
+string substitute_string(string s, string string_to_replace, string new_string) 
+{
+	for(string::size_type i = 0; (i = s.find(string_to_replace, i)) != string::npos; ) 
+	{
+		s.replace(i, string_to_replace.length(), new_string); 
+		i+=1; 
+	} 
+	return s; 
+} 
+
