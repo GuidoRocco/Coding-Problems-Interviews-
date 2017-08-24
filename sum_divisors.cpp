5f8a0174020096ac9ab44c4e869d3f8e7c681fac
Guido Rocco <guido.rocco@hotmail.it>
2017-08-24 17:02:11 +0200

    Create sum_divisors.cpp

---
+++ sum_divisors.cpp
@@ -0,0 +1,34 @@
+/* 
+Write a function that computes the number of (proper) divisors of a given integer. 
+A main function is provided in order to test the function. 
+*/ 
+
+#include <iostream> 
+#include <algorithm> 
+using namespace std; 
+
+int sum_divisors(int num) 
+{
+	if(num < 2) 
+	  return 1; 
+	int i = 1; 
+	int count = 0; 
+	while(i < num) 
+	{
+		if(num % i == 0) 
+		{
+			count += i; 
+		} 
+		
+		i++; 
+	} 
+	return count; 
+}
+
+int main(void) 
+{
+	cout << sum_divisors(10) << endl << sum_divisors(8) << endl 
+	     << sum_divisors(20) << endl << sum_divisors(100) << endl; 
+	return 0; 
+	} 
+
